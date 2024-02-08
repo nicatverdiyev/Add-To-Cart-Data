351ce7f88c67e4a1be54fe884d781951ee6bbc3e
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-02-08 19:08:22 +0400

    sebete elave etmek

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ let listCartHTML = document.querySelector('.listCart');
 let iconCartspan = document.querySelector('.icon-cart span');
 
 
-let listProduct = [];
-let carts = [];
+let product = [];
+let cart = [];
 
 // ----Sebet iconunun click'i zamanı baş verecek hadise---BAŞLANĞIC---
 iconCart.addEventListener("click", () => {
@@ -56,15 +56,59 @@ listProductHtml.addEventListener("click", (event) =>{
 });
 
 const addToCart = (product_id) => {
-    let positionThisProductInCart = carts.findIndex((value) =>{})
-    if (carts.length <= 0) {
-        carts = [{
+    let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(cart.length <= 0){
+        cart = [{
             product_id: product_id,
-            quantity:1
-        }]
+            quantity: 1
+        }];
+    }else if(positionThisProductInCart < 0){
+        cart.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    }else{
+        cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
     }
-    console.log(carts);
+    addCartToHTML();
+    addCartToMemory();
+}
+const addCartToMemory = () =>{
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    if (cart.length > 0) {
+        cart.forEach(cart => {
+            totalQuantity = totalQuantity + cart.quantity;
+            let newCart = document.createElement('div');
+            newCart.classList.add('item');
+            let positionProduct = listProduct.findIndex((value => value.id == cart.product_id));
+            let info = listProduct[positionProduct];
+            newCart.innerHTML = `
+            
+        <div class="item">
+        <div class="image">
+          <img
+            src="${info.img}"
+            alt=""
+          />
+        </div>
+        <div class="name">${info.name}</div>
+        <div class="totalPrice">${info.price * cart.quantity}</div>
+        <div class="quantity">
+          <span class="minus"> - </span>
+          <span> ${cart.quantity} </span>
+          <span class="plus"> + </span>
+        </div>
+      </div>
+            `;
+            listCartHTML.appendChild(newCart);
+        });
 
+    }
+    iconCartspan.innerText = totalQuantity;
 }
 
 const initApp = () => {
@@ -74,6 +118,11 @@ const initApp = () => {
     .then(data => {
         listProduct = data;
        addDataToHTML();
+
+    //    ---localStorage DA SAXLANMA---
+    if(localStorage.getItem('cart')){
+        cart = localStorage.getItem('cart');
+    }
     })
 
 }
